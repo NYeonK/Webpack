125379458ef3a1d3983c8eefb44ccf970f31d3b0
NYeonK <skdus728@gmail.com>
2023-01-01 19:34:40 +0900

    [ init ] webpack 세팅 및 ts, file, url-loader 추가

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        main: "./src/index.tsx",
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve("./dist"),
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                use: {
+                    loader: "ts-loader",
+                    options: {
+                        configFile: path.resolve(__dirname, "tsconfig.json"),
+                    },
+                },
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                loader: "url-loader",
+                options: {
+                    name: "[name].[ext]?[hash]",
+                    limit: 10000 // 10Kb
+                }
+            },
+        ],
+    },
+};
