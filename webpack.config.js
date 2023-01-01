4ec95c387594c01d100278388c4e0d4ac205270f
NYeonK <skdus728@gmail.com>
2023-01-01 19:42:53 +0900

    [ feat ] clean-webpack-plugin 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
     mode: "development",
@@ -34,4 +35,5 @@ module.exports = {
             },
         ],
     },
+    plugins: [new CleanWebpackPlugin()],
 };
