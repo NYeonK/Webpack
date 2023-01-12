35a0a3e9dbc95e3effc64c21d66a47d04b6842af
NYeonK <skdus728@gmail.com>
2023-01-12 21:01:10 +0900

    [ feat ] react-refresh-webpack-plugin 추가

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyPlugin = require('copy-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
     mode: "development",
@@ -64,5 +65,6 @@ module.exports = {
                 },
             ],
         }),
+        new ReactRefreshWebpackPlugin(),
     ],
 };
