394cad9aaaa4486bfedc42ccc6fceb4e7c6f723d
NYeonK <skdus728@gmail.com>
2023-02-04 23:59:19 +0900

    [ feat ] add asset file

---
+++ src/asset/image/index.tsx
@@ -0,0 +1,7 @@
+export { default as ImgBanner1 } from "./banner_1.png";
+export { default as ImgBanner2 } from "./banner_2.png";
+export { default as ImgBanner3 } from "./banner_3.png";
+export { default as ImgCategoryBanner } from "./categoryBanner.png";
+export { default as ImgDefaultBigProfile } from "./defaultBigProfile.png";
+export { default as ImgDefaultProfile } from "./defaultProfile.png";
+export { default as ImgPiickleLoading } from "./piickleLoading.gif";
