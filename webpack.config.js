aeb2c0640b06bc32b137b9ab61badc7dd649b77d
NYeonK <skdus728@gmail.com>
2023-03-02 04:53:02 +0900

    [ feat ] 서버 연결을 위한 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,81 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyPlugin = require('copy-webpack-plugin');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+dotenv.config();
 
 module.exports = {
-    mode: "development",
-    entry: {
-        main: "./src/index.tsx",
+  mode: "development",
+  entry: {
+    main: "./src/index.tsx",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    modules: ["node_modules"],
+    alias: {
+      "@": path.join(__dirname, ".", "src/"),
     },
-    output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname, "./dist"),
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx"],
-        modules: ['node_modules'],
-        alias: {
-            '@': path.join(__dirname, '..', 'src/'),
+  },
+  devServer: {
+    overlay: true,
+    stats: "errors-only",
+    hot: true,
+    host: "0.0.0.0",
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|svg|gif|jpeg|eot|svg|ttf|woff|woff2|webp)$/,
+        loader: "url-loader",
+        options: {
+          name: "[name].[ext]?[hash]",
+          limit: 10000, // 10Kb
+          outputPath: "asset",
         },
-    },
-    devServer: {
-        overlay: true,
-        stats: "errors-only",
-        hot: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|jpg|svg|gif|jpeg)$/,
-                loader: "url-loader",
-                options: {
-                    name: "[name].[ext]?[hash]",
-                    limit: 10000, // 10Kb
-                    outputPath: 'asset',
-                },
-                type: 'javascript/auto',
-            },
-            {
-                test: /\.(ts|tsx|js|jsx)$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-            },
-        ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, '.', 'public/index.html'),
-            filename: 'index.html',
-        }),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: 'public/',
-                    to: 'public/',
-                    globOptions: {
-                        ignore: ['**/*.html'],
-                    },
-                },
-            ],
-        }),
-        new ReactRefreshWebpackPlugin(),
+        type: "javascript/auto",
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
     ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, ".", "public/index.html"),
+      filename: "index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "public/",
+          to: "public/",
+          globOptions: {
+            ignore: ["**/*.html"],
+          },
+        },
+      ],
+    }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(process.env),
+    }),
+  ],
 };
