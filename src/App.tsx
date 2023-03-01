cc6f904f73a8c4c6867976e7fa54b844186877ad
NYeonK <skdus728@gmail.com>
2023-03-01 22:30:19 +0900

    [ feat ] add App.tsx and index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useRecoilValue } from "recoil";
+import styled from "styled-components";
 
-function App() {
-    return <></>;
+import { activeStateModal } from "./core/atom/menuBar";
+import Router from "./Router";
+import { ModalStyle } from "./style/modalStyle";
+import setScreenSize from "./util/setScreenSize";
+
+export default function App() {
+  const isActiveModal = useRecoilValue(activeStateModal);
+
+  useEffect(() => {
+    setScreenSize();
+  }, []);
+
+  return (
+    <St.MobileContainer>
+      <Router />
+      {isActiveModal && <ModalStyle />}
+      <div id="modal"></div>
+    </St.MobileContainer>
+  );
 }
 
-export default App;
+const St = {
+  MobileContainer: styled.div`
+    position: relative;
+
+    margin: 0 auto;
+
+    ${({ theme }) => theme.media.desktop`
+      width: 36rem;
+    `};
+  `,
+};
